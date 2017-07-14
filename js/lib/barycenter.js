d058deaae928a07c67b06b39fd59d64c2c0828fe
Ramon Saboya <ramons.gomes@hotmail.com>
2017-07-14 04:18:38 +0000

    Find barycenter coordinates by points

--- js/lib/barycenter.js
+++ js/lib/barycenter.js
@@ -1,5 +1,15 @@
-function Barycenter(beta, gamma) {
-  this.alpha = 1.0 - (beta + gamma);
+function Barycenter(alpha, beta, gamma) {
+  this.alpha = alpha;
   this.beta = beta;
   this.gamma = gamma;
+}
+
+function findBarycenter(x, y, p1, p2, p3) {
+  var denom = ((p1.x - p3.x) * (p2.y - p3.y)) - ((p1.y - p3.y) * (p2.x - p3.x));
+  
+	var	alpha = ((p2.y - p3.y) * (x - p3.x) - ((p3.x - p2.x) * (p3.y - y))) / denom;
+	var	beta = (((p3.y - p1.y) * (x - p3.x)) - ((p1.x - p3.x) * (p3.y - y))) / denom;
+	var	gamma = 1.0 - alpha - beta;
+	
+  return new Barycenter(alpha, beta, gamma);
 }
